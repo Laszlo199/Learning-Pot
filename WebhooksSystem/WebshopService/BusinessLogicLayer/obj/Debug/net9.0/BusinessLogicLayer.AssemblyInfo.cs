5933154bedff599d53ba3fcb2c31682a84a642a9
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-01 12:19:53 +0100

    move the entities in the business layer

--- WebhooksSystem/WebshopService/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7c36610c3ecefebc33fe8627d47dd4fec1dc899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfa8fd36b4d897f56c0512271f037f731da965d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebhooksSystem/WebshopService/DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa2e5fdf6f700f4e687099997627f95e72e1d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfa8fd36b4d897f56c0512271f037f731da965d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
