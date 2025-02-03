0b51b04cebdff08a59222d966be77324467a4474
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-03 13:53:34 +0100

    add 2 minimal APIs

--- WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46a39bd7d5d2763ed3f940b52994bf33019810f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4315914e0845ad8feff030031b8d94eff35e06e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
