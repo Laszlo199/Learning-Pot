8b1754eda01e4d1c25da4460248273fb556c2b31
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-03 15:33:30 +0100

    update entities

--- WebhooksSystem/WebshopService/BusinessLogicLayer/Entities/Order.cs
+++ WebhooksSystem/WebshopService/BusinessLogicLayer/Entities/Order.cs
@@ -4,5 +4,9 @@ namespace DataAccessLayer.Entities;
 
 public class Order
 {
-
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+    public Guid CustomerId { get; set; }
+    public Customer Customer { get; set; }
 }
--- WebhooksSystem/WebshopService/BusinessLogicLayer/Entities/OrderItem.cs
+++ WebhooksSystem/WebshopService/BusinessLogicLayer/Entities/OrderItem.cs
@@ -4,9 +4,11 @@ namespace DataAccessLayer.Entities;
 
 public class OrderItem
 {
-    public int OrderItemId { get; set; }
+    public Guid OrderItemId { get; set; }
     public int OrderId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
     public decimal Price { get; set; }
+    public Order Order { get; set; }
+    public Product Product { get; set; }
 }
