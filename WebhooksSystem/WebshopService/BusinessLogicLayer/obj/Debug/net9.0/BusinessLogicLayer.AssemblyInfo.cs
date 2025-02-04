0ef5dea48bd90ef4e21892924b9bb99d4f852b4c
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-04 20:08:42 +0100

    changes

--- WebhooksSystem/WebshopService/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bd22f363968caabf66184a6e6ebad8cb22a269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37c397f4d4e92d1104b20c0f6e6645c22c6de2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
