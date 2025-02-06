795708da282512ebf47ceddce5a440ca44abeaec
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-06 15:38:03 +0100

    delete old migration

--- WebhooksSystem/WebshopService/DataAccessLayer/WebShopDbContext.cs
+++ WebhooksSystem/WebshopService/DataAccessLayer/WebShopDbContext.cs
@@ -10,9 +10,6 @@ public class WebShopDbContext : DbContext
     {
     }
 
-    public DbSet<Customer> Customers {get; set;}
     public DbSet<Order> Orders {get; set;}
-    public DbSet<OrderItem> OrderItems {get; set;}
-    public DbSet<Product> Products {get; set;}
     public DbSet<WebhookSubscription> WebhookSubscriptions {get; set;}
 }
--- WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e41a07d5398c0aa0734554dbba41396e96347c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479de7b98257273a9712826f6b66ef786cd5a666")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
