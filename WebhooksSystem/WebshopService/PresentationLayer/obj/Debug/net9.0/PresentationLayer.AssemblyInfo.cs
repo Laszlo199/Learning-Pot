fd01eb6cad73a930a5ac024db3b43f4ef850c5e3
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-06 13:31:15 +0100

    hm

--- WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ WebhooksSystem/WebshopService/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527aed5606aeb60c87f19d3823e6ac9196e0940e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a3e64dd58d302e362e1a32a0cbec1d6b1dbddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
