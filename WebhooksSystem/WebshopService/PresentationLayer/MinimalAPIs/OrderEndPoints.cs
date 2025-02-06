479de7b98257273a9712826f6b66ef786cd5a666
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-06 15:17:40 +0100

    Update OrderEndPoints.cs

--- WebhooksSystem/WebshopService/PresentationLayer/MinimalAPIs/OrderEndPoints.cs
+++ WebhooksSystem/WebshopService/PresentationLayer/MinimalAPIs/OrderEndPoints.cs
@@ -31,7 +31,7 @@ public static void MapOrderEndPoints(this IEndpointRouteBuilder app)
 
         await service.CreateOrder(newOrder);
 
-        return TypedResults.Created($"Orders send successfully.");
+        return TypedResults.Created($"Order created successfully for {order.CostumerName}.");
     }
     
 }
