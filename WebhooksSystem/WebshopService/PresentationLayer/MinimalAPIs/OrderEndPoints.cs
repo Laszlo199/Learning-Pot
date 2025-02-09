4b5b9480ac6508d6fefd673c8d8ded9b1e5a81ee
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2025-02-09 13:16:49 +0100

    Update OrderEndPoints.cs

--- WebhooksSystem/WebshopService/PresentationLayer/MinimalAPIs/OrderEndPoints.cs
+++ WebhooksSystem/WebshopService/PresentationLayer/MinimalAPIs/OrderEndPoints.cs
@@ -8,10 +8,10 @@ public static class OrderEndPoints
 {
 public static void MapOrderEndPoints(this IEndpointRouteBuilder app)
     {
-        var product = app.MapGroup("orders");
+        var order = app.MapGroup("orders");
 
-        product.MapGet("/", GetAll);
-        product.MapPost("/", Create);
+        order.MapGet("/", GetAll);
+        order.MapPost("/", Create);
     }
 
     static async Task<IResult> GetAll(IOrderService service)
